df7ea4b5540b5af6c88006a50675a74b694ae018
Andrew Hammett <hammettx@amazon.com>
2024-02-22 23:55:19 +0000

    httpd to apache2 for ubuntu22

--- examples/ec2-gold-image/scripts/userdata.sh
+++ examples/ec2-gold-image/scripts/userdata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt update -y
-apt install -y httpd
-systemctl start httpd.service
-systemctl enable httpd.service
+apt install -y apache2
+systemctl start apache2.service
+systemctl enable apache2.service
 echo "Gold $(hostname -f)" > /var/www/html/index.html
+
+echo "end"
