545037b962a95a97385b80fcb7b5ed56da6d7084
Andrew Hammett <hammettx@amazon.com>
2024-02-22 23:54:49 +0000

    enable more access for debugging

--- examples/ec2-gold-image/main.tf
+++ examples/ec2-gold-image/main.tf
@@ -16,10 +16,71 @@ resource "aws_subnet" "primary_subnet" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name} igw"
+  }
+}
+
+resource "aws_route_table" "routes" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.project_name} routes"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = 1
+  subnet_id      = aws_subnet.primary_subnet.id
+  route_table_id = aws_route_table.routes.id
+}
+
+# enabled for validation
+data "aws_key_pair" "example" {
+  key_name           = "example"
+  include_public_key = true
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name   = "public web ssh"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = -1
+}
+
 resource "aws_instance" "gold" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.primary_subnet.id
+  # enabled for validation
+  key_name      = data.aws_key_pair.example.key_name
+
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
 
   user_data_replace_on_change = true
   user_data                   = file("scripts/userdata.sh")
